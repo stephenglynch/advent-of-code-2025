b3e351309ba2d73b8ab5bc319e8506c17aa6bfa6
Stephen Lynch <stepheng.lynch@gmail.com>
2025-12-23 23:48:09 +0000

    Ooops need to bring back my working part 2

--- src/day1p2.rs
+++ src/day1p2.rs
@@ -1,5 +1,3 @@
-use std::ops;
-
 const INPUT: &str = include_str!("../data/day1/input.txt");
 
 struct Wrapping100 {
@@ -14,27 +12,24 @@ impl Wrapping100 {
             zero_xs: 0
         }
     }
-}
 
-impl ops::Add for Wrapping100 {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self::Output {
-        Wrapping100 {
-            value: (self.value + rhs.value) % 100,
-            zero_xs: self.zero_xs + (self.value + rhs.value / 100)
+    fn right(&mut self, value: i32) {
+        for _ in 0..value {
+            self.value = self.value + 1;
+            if self.value == 100 {
+                self.value = 0;
+                self.zero_xs = self.zero_xs + 1;
+            }
         }
     }
-}
 
-impl ops::Sub for Wrapping100 {
-    type Output = Self;
-    fn sub(self, rhs: Self) -> Self::Output {
-        Wrapping100 {
-            value: (self.value - rhs.value) % 100,
-            zero_xs: if self.value - rhs.value <= 0 {
-                self.zero_xs + (self.value - rhs.value).abs() / 100 + 1
-            } else {
-                self.zero_xs
+    fn left(&mut self, value: i32) {
+        for _ in 0..value {
+            self.value = self.value - 1;
+            if self.value == 0 {
+                self.zero_xs = self.zero_xs + 1;
+            } else if self.value == -1 {
+                self.value = 99;
             }
         }
     }
@@ -42,15 +37,14 @@ impl ops::Sub for Wrapping100 {
 
 pub fn run() {
     let mut clock = Wrapping100::new(50);
-    let mut answer = 0;
 
     for line in INPUT.lines() {
         let prefix = line.chars().nth(0).unwrap();
         let num = line[1..].parse().unwrap();
         if prefix == 'L' {
-            clock = clock - Wrapping100::new(num);
+            clock.left(num);
         } else {
-            clock = clock + Wrapping100::new(num);
+            clock.right(num);
         }
     }
 
