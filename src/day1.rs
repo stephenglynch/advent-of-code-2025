ec151163d73cb4b381100e18a02e01b7f3165536
Stephen Lynch <stepheng.lynch@gmail.com>
2025-12-23 22:07:35 +0000

    Attempt to be clever

--- src/day1.rs
+++ src/day1.rs
@@ -2,39 +2,57 @@ use std::ops;
 
 const INPUT: &str = include_str!("../data/day1/input.txt");
 
-struct Wrapping100(i16);
+struct Wrapping100 {
+    value: i32,
+    zero_xs: i32
+}
+
+impl Wrapping100 {
+    fn new(value: i32) -> Wrapping100 {
+        Wrapping100 {
+            value: value,
+            zero_xs: 0
+        }
+    }
+}
 
 impl ops::Add for Wrapping100 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        Wrapping100((self.0 + rhs.0) % 100)
+        Wrapping100 {
+            value: (self.value + rhs.value) % 100,
+            zero_xs: self.zero_xs + (self.value + rhs.value / 100)
+        }
     }
 }
 
 impl ops::Sub for Wrapping100 {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
-        Wrapping100((self.0 - rhs.0) % 100)
+        Wrapping100 {
+            value: (self.value - rhs.value) % 100,
+            zero_xs: if self.value - rhs.value <= 0 {
+                self.zero_xs + (self.value - rhs.value).abs() / 100 + 1
+            } else {
+                self.zero_xs
+            }
+        }
     }
 }
 
-pub fn part1() {
-    let mut clock = Wrapping100(50);
+pub fn part2() {
+    let mut clock = Wrapping100::new(50);
     let mut answer = 0;
 
     for line in INPUT.lines() {
         let prefix = line.chars().nth(0).unwrap();
         let num = line[1..].parse().unwrap();
         if prefix == 'L' {
-            clock = clock - Wrapping100(num);
+            clock = clock - Wrapping100::new(num);
         } else {
-            clock = clock + Wrapping100(num);
-        }
-
-        if clock.0 == 0 {
-            answer = answer + 1;
+            clock = clock + Wrapping100::new(num);
         }
     }
 
-    println!("answer = {}", answer);
+    println!("answer = {}", clock.zero_xs);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@
 mod day1;
 
 fn main() {
-    day1::part1();
+    day1::part2();
 }
